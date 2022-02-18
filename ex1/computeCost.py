62f8b5d4f05c83c951e37d205f98712eabbf88d9
zyj <1305696765@qq.com>
2022-02-18 23:55:45 +0800

    使用Jupyter Notebook

--- ex1/computeCost.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# 向量换实现的代价函数计算
-# X:(m, n)
-# theta:(n, 1)
-def computeCost(X, y, theta):
-    m = len(X)
-    z = np.dot(X, theta) - y
-    return np.sum(np.square(z)) / (2 * m)
--- ex2/auxiliary.py
+++
@@ -1,6 +0,0 @@
-# 辅助函数
-import numpy as np
-
-def get_X_y(data):
-    return [data.iloc[:, :-1], data.iloc[:, -1]]
-
